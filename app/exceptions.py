5bee108a38f9c2cbc379f476277da934c5b5e10b
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2026-05-22 14:44:46 +0400

    add base class

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,2 +1,12 @@
-class RiotAPIError(Exception):
+class AppError(Exception):
+    """Base class for app errors."""
+
+    pass
+
+
+class RiotAPIError(AppError):
+    pass
+
+
+class PlayerNotFoundError(AppError):
     pass

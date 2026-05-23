242dd7998d134dfe3ce0ad241e57a860ee85dea4
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2026-05-23 19:27:00 +0400

    add background task to player update

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,12 +1,7 @@
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
 from app.infrastructure.repositories.player_repo import PlayerRepository
-from fastapi import APIRouter, Depends, HTTPException
-from app.services.match_service import MatchService
-from app.services.ranked_service import RankedEntriesService
-from app.dependencies import (
-    get_player_repo,
-    get_match_service,
-    get_ranked_entries_service,
-)
+from app.tasks import update_player_data_task
+from app.dependencies import get_player_repo
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
@@ -14,15 +9,17 @@ router = APIRouter(prefix="/admin", tags=["admin"])
 @router.post("/players/{puuid}/update")
 async def update_player_matches(
     puuid: str,
-    match_service: MatchService = Depends(get_match_service),
-    ranked_service: RankedEntriesService = Depends(get_ranked_entries_service),
+    background_tasks: BackgroundTasks,
     player_repo: PlayerRepository = Depends(get_player_repo),
-):
+) -> dict[str, str]:
+    """Обновляет ranked entries и историю матчей игрока."""
     player = await player_repo.get_by_puuid(puuid)
     if not player:
         raise HTTPException(status_code=404, detail="Player not found")
 
-    await ranked_service.update_ranked_entries(player)
-    await match_service.update_player_matches(player)
+    background_tasks.add_task(
+        update_player_data_task,
+        puuid,
+    )
 
-    return {"status": "player was updated"}
+    return {"status": "player started updated in the background"}
---
+++ app/tasks.py
@@ -0,0 +1,28 @@
+from app.infrastructure.db.database import async_session
+from app.infrastructure.repositories.player_repo import PlayerRepository
+from app.services.match_service import MatchService
+from app.services.ranked_service import RankedEntriesService
+from app.dependencies import riot_client
+
+
+async def update_player_data_task(puuid: str) -> None:
+    """Фоновая задача обновления данных игрока."""
+
+    async with async_session() as db:
+        player_repo = PlayerRepository(db)
+        player = await player_repo.get_by_puuid(puuid)
+        if not player:
+            return
+
+        ranked_service = RankedEntriesService(
+            db=db,
+            riot_client=riot_client,
+        )
+
+        match_service = MatchService(
+            db=db,
+            riot_client=riot_client,
+        )
+
+        await ranked_service.update_ranked_entries(player)
+        await match_service.update_player_matches(player)
